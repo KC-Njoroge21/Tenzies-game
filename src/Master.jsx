36ef456999e73c1a995de89bad0e5c9661de95b5
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2025-09-30 16:38:10 +0300

    Added New Game Button

--- src/Master.jsx
+++ src/Master.jsx
@@ -7,7 +7,8 @@ const Master = () => {
   const [numbers, setNumbers] = React.useState(generateRandomNumber())
 
   function rollDice() {
-    setNumbers((prevState) => {
+   if (!gameWon) {
+     setNumbers((prevState) => {
       return(
          prevState.map((item) => {
           return (
@@ -17,6 +18,9 @@ const Master = () => {
       )
        
     })
+   } else {
+    setNumbers(generateRandomNumber)
+   }
   }
 
   function holdDice(id) {
@@ -47,7 +51,11 @@ const Master = () => {
   })
 
   
-  
+  const gameWon = numbers.every((number) => {
+      return number.isHeld
+    }) && numbers.every((number) => {
+      return number.value === numbers[0].value
+    })
 
   
  
@@ -56,6 +64,7 @@ const Master = () => {
   return (
     <section className="bg-blue-950 p-4 h-screen flex flex-col justify-center items-center">
       <div className="bg-[#F5F5F5] h-full rounded-xl max-h-[400px] max-w-[400px] w-full flex flex-col justify-center items-center gap-6 p-3 ">
+       
         <h1 className="text-4xl font-semibold">Tenzies</h1>
         <p className="text-center">
           Roll until all dice are the same. Click each die to freeze it at its currrent value between rolls.
@@ -63,7 +72,7 @@ const Master = () => {
         <div className="grid grid-cols-5 grid-rows-2 gap-4">
           {die}
         </div>
-        <button onClick={rollDice}  className="bg-blue-950 text-white px-4 py-2 rounded-md mt-4 font-semibold cursor-pointer">Roll</button>
+        <button onClick={rollDice}  className="bg-blue-950 text-white px-4 py-2 rounded-md mt-4 font-semibold cursor-pointer">{gameWon ? "New Game" : "Roll"}</button>
       </div>
     </section>
   );
